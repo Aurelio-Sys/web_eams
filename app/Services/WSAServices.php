8c536bb0ed755f6cf5c3be7d9ac3a761d80f0ea2
tommywcak <tomy0344@gmail.com>
2022-09-01 10:25:49 +0700

    Buat Services dan error view default layout

---
+++ app/Services/WSAServices.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+class WSAServices
+{
+    private function httpHeader($req)
+    {
+        return array(
+            'Content-type: text/xml;charset="utf-8"',
+            'Accept: text/xml',
+            'Cache-Control: no-cache',
+            'Pragma: no-cache',
+            'SOAPAction: ""',        // jika tidak pakai SOAPAction, isinya harus ada tanda petik 2 --> ""
+            'Content-length: ' . strlen(preg_replace("/\s+/", " ", $req))
+        );
+    }
+    //
+}
