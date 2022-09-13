87b562712539a6bfd6d1c819ea3f9ce22daeb9d5
tommywcak <tomy0344@gmail.com>
2022-09-13 16:17:33 +0700

    perubahan tommy 13092022

--- app/Http/Controllers/WO/WORelease.php
+++ app/Http/Controllers/WO/WORelease.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\WO;
 
 use App\Http\Controllers\Controller;
+use App\Services\CreateTempTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class WORelease extends Controller
 {
@@ -54,26 +56,56 @@ class WORelease extends Controller
                     ->get();
 
         if($data->wo_repair_code1 != ""){
-
-        }
-
-        if($data->wo_repair_code2 != ""){
-
-        }
-
-        if($data->wo_repair_code3 != ""){
-
-        }
-
-        $sparepart1 = DB::table('wo_mstr')
+            $sparepart1 = DB::table('wo_mstr')
+                        ->select('insd_det.*', DB::raw('sum(insd_det.insd_qty) as insd_req'))
                         ->leftJoin('rep_master','wo_mstr.wo_repair_code1','rep_master.repm_code')
                         ->leftJoin('rep_det','rep_master.repm_code','rep_det.repdet_code')
                         ->leftJoin('ins_mstr','rep_det.repdet_ins','ins_mstr.ins_code')
+                        ->leftJoin('insd_det','ins_mstr.ins_code','insd_det.insd_code')
                         ->where('wo_id','=',$id)
+                        ->groupBy('insd_det.insd_part')
                         ->get();
 
-        // dd($sparepart1);
+            $tempSP1 = (new CreateTempTable())->createSparePartUsed($sparepart1);
+        }
 
+        if($data->wo_repair_code2 != ""){
+            $sparepart2 = DB::table('wo_mstr')
+                        ->select('insd_det.*', DB::raw('sum(insd_det.insd_qty) as insd_req'))
+                        ->leftJoin('rep_master','wo_mstr.wo_repair_code2','rep_master.repm_code')
+                        ->leftJoin('rep_det','rep_master.repm_code','rep_det.repdet_code')
+                        ->leftJoin('ins_mstr','rep_det.repdet_ins','ins_mstr.ins_code')
+                        ->leftJoin('insd_det','ins_mstr.ins_code','insd_det.insd_code')
+                        ->where('wo_id','=',$id)
+                        ->groupBy('insd_det.insd_part')
+                        ->get();
+            
+            $tempSP2 = (new CreateTempTable())->createSparePartUsed($sparepart2);
+        }
+
+        if($data->wo_repair_code3 != ""){
+            $sparepart3 = DB::table('wo_mstr')
+                        ->select('insd_det.*', DB::raw('sum(insd_det.insd_qty) as insd_req'))
+                        ->leftJoin('rep_master','wo_mstr.wo_repair_code3','rep_master.repm_code')
+                        ->leftJoin('rep_det','rep_master.repm_code','rep_det.repdet_code')
+                        ->leftJoin('ins_mstr','rep_det.repdet_ins','ins_mstr.ins_code')
+                        ->leftJoin('insd_det','ins_mstr.ins_code','insd_det.insd_code')
+                        ->where('wo_id','=',$id)
+                        ->groupBy('insd_det.insd_part')
+                        ->get();
+
+            $tempSP3 = (new CreateTempTable())->createSparePartUsed($sparepart3);
+        }
+
+        $table_sparepart = DB::table('temp_sp')->get();
+
+        Schema::dropIfExists('temp_sp');
+
+        dd($table_sparepart);
+
+
+
+        
         return view('workorder.worelease-detail', compact('data','spdata'));
     }
     
