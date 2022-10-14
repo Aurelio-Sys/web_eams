988a91ba55dd428b97ee8296dfcb1328e3abc8fb
tyasmpir <87292193+tyasmpir@users.noreply.github.com>
2022-10-14 16:59:21 +0700

    2022.10.14

---
+++ resources/views/report/prevsch.blade.php
@@ -0,0 +1,190 @@
+@extends('layout.newlayout')
+@section('content-header')
+      <div class="container-fluid">
+        <div class="row">          
+          <div class="col-sm-4">
+            <h1 class="m-0 text-dark">Preventive Schedule</h1>
+          </div>    
+        </div><!-- /.row -->
+        <div class="col-md-12">
+          <hr>
+        </div>
+      </div><!-- /.container-fluid -->
+@endsection
+@section('content')
+
+<!--FORM Search Disini -->
+<div class="container-fluid mb-2">
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+<li class="nav-item has-treeview bg-black">
+<a href="#" class="nav-link mb-0 p-0"> 
+<p>
+  <label class="col-md-2 col-form-label text-md-left" style="color:white;">{{ __('Click here to search') }}</label>
+  <i class="right fas fa-angle-left"></i>
+</p>
+</a>
+<ul class="nav nav-treeview">
+<li class="nav-item">
+<div class="col-12 form-group row">
+    <div class="col-12 form-group row">
+        <label for="s_code" class="col-md-2 col-sm-2 col-form-label text-md-right">Department Code</label>
+        <div class="col-md-4 mb-2 input-group">
+            <input id="s_code" type="text" class="form-control" name="s_code"
+            value="" autofocus autocomplete="off"/>
+        </div>
+        <label for="s_desc" class="col-md-2 col-sm-2 col-form-label text-md-right">Department Description</label>
+        <div class="col-md-4 mb-2 input-group">
+            <input id="s_desc" type="text" class="form-control" name="s_desc"
+            value="" autofocus autocomplete="off"/>
+        </div>
+        <label for="btnsearch" class="col-md-2 col-sm-2 col-form-label text-md-left">{{ __('') }}</label>
+        <div class="col-md-2 mb-2 input-group">
+            <input type="button" class="btn btn-block btn-primary" id="btnsearch" value="Search" />
+        </div>
+        <div class="col-md-2 col-sm-12 mb-2 input-group">
+            <button class="btn btn-block btn-primary" style="width: 40px !important" id='btnrefresh' /><i class="fas fa-sync-alt"></i></button>
+        </div>
+        <input type="hidden" id="tmpcode"/>
+        <input type="hidden" id="tmpdesc"/>
+    </div>
+</div>
+</li>
+</ul>
+</li>
+</ul>
+</div>
+
+<!-- Bagian Searching -->
+<div class="col-md-12"><hr></div>
+
+<div class="table-responsive col-12">
+    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+            <tr>
+                <th width="30%">Asset</th>
+                <th width="60%">Description</th>
+                <th width="10%">Last Maintenance</th>  
+                <th width="10%">Measure</th>  
+                <th width="10%">January</th>  
+                <th width="10%">February</th>  
+                <th width="10%">March</th>  
+                <th width="10%">April</th>  
+                <th width="10%">May</th>  
+                <th width="10%">June</th>  
+                <th width="10%">July</th>  
+                <th width="10%">August</th>  
+                <th width="10%">September</th>  
+                <th width="10%">October</th>  
+                <th width="10%">November</th>  
+                <th width="10%">December</th>  
+                 
+            </tr>
+        </thead>
+        <tbody>
+            <!-- untuk isi table -->
+            <!-- @include('setting.table-departemen') -->
+        </tbody>
+    </table>
+    <input type="hidden" name="hidden_page" id="hidden_page" value="1"/>
+    <input type="hidden" name="hidden_column_name" id="hidden_column_name" value="dept_code"/>
+    <input type="hidden" name="hidden_sort_type" id="hidden_sort_type" value="asc" />
+</div>
+
+
+@endsection
+
+@section('scripts')
+
+       function clear_icon()
+       {
+            $('#id_icon').html('');
+            $('#post_title_icon').html('');
+       }
+
+       function fetch_data(page, sort_type, sort_by, code, desc){
+            $.ajax({
+                url:"deptmaster/pagination?page="+page+"&sorttype="+sort_type+"&sortby="+sort_by+"&code="+code+"&desc="+desc,
+                success:function(data){
+                    console.log(data);
+                    $('tbody').html('');
+                    $('tbody').html(data);
+                }
+            })
+        }
+
+        $(document).on('click', '#btnsearch', function(){
+
+            var code = $('#s_code').val();
+            var desc = $('#s_desc').val();
+            var column_name = $('#hidden_column_name').val();
+			var sort_type = $('#hidden_sort_type').val();
+            var page = 1;
+            
+            document.getElementById('tmpcode').value = code;
+            document.getElementById('tmpdesc').value = desc;
+
+            fetch_data(page, sort_type, column_name, code, desc);
+        });
+
+       $(document).on('click', '.sorting', function(){
+			var column_name = $(this).data('column_name');
+			var order_type = $(this).data('sorting_type');
+			var reverse_order = '';
+			if(order_type == 'asc')
+			{
+			$(this).data('sorting_type', 'desc');
+			reverse_order = 'desc';
+			clear_icon();
+			$('#'+column_name+'_icon').html('<span class="glyphicon glyphicon-triangle-bottom"></span>');
+			}
+			if(order_type == 'desc')
+			{
+			$(this).data('sorting_type', 'asc');
+			reverse_order = 'asc';
+			clear_icon();
+			$('#'+column_name+'_icon').html('<span class="glyphicon glyphicon-triangle-top"></span>');
+			}
+			$('#hidden_column_name').val(column_name);
+			$('#hidden_sort_type').val(reverse_order);
+            var page = $('#hidden_page').val();
+            var code = $('#s_code').val();
+            var desc = $('#s_desc').val();
+			fetch_data(page, reverse_order, column_name, code, desc);
+     	});
+       
+       
+       $(document).on('click', '.pagination a', function(event){
+            event.preventDefault();
+            var page = $(this).attr('href').split('page=')[1];
+            $('#hidden_page').val(page);
+            var column_name = $('#hidden_column_name').val();
+            var sort_type = $('#hidden_sort_type').val();
+            var code = $('#s_code').val();
+            var desc = $('#s_desc').val();
+            fetch_data(page, sort_type, column_name, code, desc);
+       });
+
+       $(document).on('click', '#btnrefresh', function() {
+
+            var code  = ''; 
+            var desc = '';
+
+            var column_name = $('#hidden_column_name').val();
+            var sort_type = $('#hidden_sort_type').val();
+            var page = 1;
+
+            document.getElementById('s_code').value  = '';
+            document.getElementById('s_desc').value  = '';
+            document.getElementById('tmpcode').value  = code;
+            document.getElementById('tmpdesc').value  = desc;
+
+            fetch_data(page, sort_type, column_name, code, desc);
+        });
+
+        
+    </script>
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/css/select2.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/js/select2.min.js"></script>
+
+@endsection
