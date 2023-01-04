37ef6272f97c23c16f934c8396b872151180ba40
tommywcak <tomy0344@gmail.com>
2023-01-04 23:14:36 +0700

    update kecil

--- app/Http/Controllers/wocontroller.php
+++ app/Http/Controllers/wocontroller.php
@@ -3422,7 +3422,7 @@ class wocontroller extends Controller
 
             $costdata = (new WSAServices())->wsacost($domain->wsas_domain);
 
-            if ($costdata) {
+            if ($costdata === false) {
                 alert()->error('Error', 'WSA Failed');
                 return redirect()->route('woreport');
             } else {
