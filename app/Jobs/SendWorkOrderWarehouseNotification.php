547293179cb0e0bd53d5ffff5b4fec6d24327a7e
tommywcak <tomy0344@gmail.com>
2023-04-27 13:58:22 +0700

    100% wo released

--- app/Jobs/SendWorkOrderWarehouseNotification.php
+++ app/Jobs/SendWorkOrderWarehouseNotification.php
@@ -7,6 +7,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App;
 
 class SendWorkOrderWarehouseNotification implements ShouldQueue
 {
@@ -35,6 +38,47 @@ class SendWorkOrderWarehouseNotification implements ShouldQueue
     {
         //
 
-        $wonumber
+        $wonumber = $this->wonumber;
+
+        $getUserWH = DB::table('users')
+                        ->where('role_user', '=', 'WHS')
+                        ->where('active','=', 'Yes')
+                        ->get();
+
+        $emails = '';
+
+        foreach ($getUserWH as $email) {
+            $emails .= $email->email_user . ',';
+        }
+
+        $emails = substr($emails, 0, strlen($emails) - 1);
+        
+        $array_email = explode(',', $emails);
+        
+        if ($getUserWH->count()){
+            Mail::send('emailsendtowh',
+                [
+                    'wonumber' => $wonumber,
+                ],function ($message) use($array_email,$wonumber){
+                        $message->subject('eAMS - Work Order Transfer Needed for '.$wonumber.'');
+                        $message->to($array_email);
+                });
+
+
+            foreach ($getUserWH as $approver) {
+                $user = App\User::where('id', '=', $approver->id)->first();
+                $details = [
+                    'body' => 'Work Order Transfer Needed',
+                    'url' => 'womaint',
+                    'nbr' => $wonumber,
+                    'note' => 'Please check'
+
+                ]; // isi data yang dioper
+
+                $user->notify(new \App\Notifications\eventNotification($details)); // syntax laravel                
+
+            }
+        }
+        
     }
 }
---
+++ resources/views/emailsendtowh.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Work Order Cancellation Notification</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            line-height: 1.4;
+            background-color: #f7f7f7;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, .1);
+        }
+
+        .header {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .header h1 {
+            margin: 0;
+            font-size: 24px;
+        }
+
+        .content {
+            margin-bottom: 30px;
+        }
+
+        .footer {
+            text-align: center;
+            font-size: 12px;
+            color: #999;
+            border-top: 1px solid #eee;
+            padding-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Work Order Spare Part Transfer</h1>
+        </div>
+        <div class="content">
+            <p>The work order number {{$wonumber}} requires an immediate transfer of spare parts.</p>
+        </div>
+        <div class="footer">
+        </div>
+    </div>
+</body>
+
+</html>
