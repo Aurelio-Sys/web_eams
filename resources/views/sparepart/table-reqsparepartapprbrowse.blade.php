320750e8b9bc1931f67fb0c1bc6ebd4663a6fc5a
mirantieka <mirantiekaa4@gmail.com>
2023-08-31 15:23:14 +0700

    perubahan dk

--- resources/views/sparepart/table-reqsparepartapprbrowse.blade.php
+++ resources/views/sparepart/table-reqsparepartapprbrowse.blade.php
@@ -12,7 +12,7 @@
     <td style="text-align: center;">
         <!-- approval -->
         @if($datas->getCurrentApprover != null)
-        @if($datas->getCurrentApprover->rqtr_role_approval == session('role') || session('role') == 'ADMIN')
+        @if($datas->getCurrentApprover->rqtr_role_approval == session('role') || session('role') == 'ADMIN' && $datas->rqtr_status == 'waiting for approval')
         <a class="approvalreqsp" href="javascript:void(0)" type="button" data-toggle="tooltip" title="Approval Request Sparepart" data-rsnumber="{{ $datas->req_sp_number }}" data-wonumber="{{ $datas->req_sp_wonumber }}" data-reqby="{{ $datas->req_sp_requested_by }}" data-duedate="{{ $datas->req_sp_due_date == '0000-00-00' ? '' : $datas->req_sp_due_date }}" data-trfby="{{ $datas->req_sp_transfered_by }}" data-trfdate="{{ $datas->req_sp_transfer_date == '0000-00-00' ? '' : date('m/d/Y', strtotime($datas->req_sp_transfer_date)) }}" data-status="{{ $datas->req_sp_status }}" data-spcode="{{ $datas->req_spd_sparepart_code }}" data-spdesc="{{ $datas->spm_desc }}" data-qtyreq="{{ $datas->req_spd_qty_request }}" data-qtytrf="{{ $datas->req_spd_qty_transfer }}" data-locfrom="{{ $datas->req_spd_loc_from }}" data-locto="{{ $datas->req_spd_loc_to }}" data-note="{{ $datas->req_spd_note }}"><i class="icon-table fas fa-thumbs-up fa-lg"></i></a>
         @else
         <!-- view -->
