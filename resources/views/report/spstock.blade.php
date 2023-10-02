123079203c821356ae04eaf4a2bf77f78322b58b
tommywcak <tomy0344@gmail.com>
2023-10-02 15:35:23 +0700

    perbaikan menu inventory stock sp R1

--- resources/views/report/spstock.blade.php
+++ resources/views/report/spstock.blade.php
@@ -2,7 +2,7 @@
 @section('content-header')
 <div class="container-fluid">
   <div class="row">
-    <div class="col-sm-4">
+    <div class="col-sm-6">
       <h1 class="m-0 text-dark">Spare Part Stock Report</h1>
     </div>
   </div><!-- /.row -->
